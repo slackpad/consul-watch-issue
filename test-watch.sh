22204dbf82ba9eda450e760b74dfe4cfdd3796fe
strarsis <strarsis@gmail.com>
2016-09-24 02:33:10 +0200

    Add log trace option to watcher invokation.

--- test-watch.sh
+++ test-watch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --rm -ti --link consulwatchissue_consul_1 consul:v0.7.0 consul agent -data-dir /consul/data -join=CONSULWATCHISSUE_CONSUL_1 -node test watch -type keyprefix -prefix foo/
+docker run --rm -ti --link consulwatchissue_consul_1 consul:v0.7.0 consul agent -data-dir /consul/data -join=CONSULWATCHISSUE_CONSUL_1 -node test -log-level=TRACE watch -type keyprefix -prefix foo/
